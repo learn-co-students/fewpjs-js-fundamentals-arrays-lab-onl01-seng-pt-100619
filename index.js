b246099f31bc6c923591da865b3c72e1194c0dcb
allisoncortez <allizetorc@gmail.com>
2020-04-27 12:38:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution here!
+// let newLength = fruits.push('Orange')
+// const append = [ '',];
+
+const append = ["Milo", "Otis", "Garfield"];
+
+const prepend = ["Milo", "Otis", "Garfield"];
+
+const removeLast = ["Milo", "Otis", "Garfield"];
+
+const removeFirst = ["Milo", "Otis", "Garfield"];
+
+
+// add to the end of array
+append.push("Odie");
+// add to the beginning of array
+prepend.unshift("Odie");
+
+// remove the last element in array
+removeLast.pop("Odie");
+// remove the first element in array
+removeFirst.shift("Odie");
