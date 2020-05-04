a4c79adbc8486b2b2ffd83ab93d46b7ad17f99cb
emanningt <emanningmusic@yahoo.com>
2020-05-04 09:51:46 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution here!
+
+const append = ["Milo", "Otis", "Garfield"]
+
+append.push("Odie")
+
+const prepend = ["Milo", "Otis", "Garfield"]
+
+prepend.unshift("Odie")
+
+const removeLast = ["Milo", "Otis", "Garfield"].slice(0,2)
+
+const removeFirst = ["Milo", "Otis", "Garfield"].slice(1,3)
